b7f213cc32e652438d82f19e66f3a70ceccd884d
Nihad Delic <nihad@upday.com>
2020-11-21 09:24:19 +0100

    WIP

--- test/unit/src/web_view_widget_parameters_test.dart
+++ test/unit/src/web_view_widget_parameters_test.dart
@@ -82,4 +82,73 @@ void main() {
       expect(isUrlRedirection, isFalse);
     });
   });
+
+  group('AuthCodeConfig class', () {
+    test('Is created', () {
+      AuthCodeConfig(
+        redirectUrl: 'N/A',
+        clientId: 'N/A',
+      );
+    });
+
+    test('Throws assertion if redirect url is null', () {
+      expect(
+        () => AuthCodeConfig(
+          redirectUrl: null,
+          clientId: 'N/A',
+        ),
+        throwsAssertionError,
+      );
+    });
+
+    test('Throws assertion if client id is null', () {
+      expect(
+        () => AuthCodeConfig(
+          redirectUrl: 'N/A',
+          clientId: null,
+        ),
+        throwsAssertionError,
+      );
+    });
+
+    test(
+        'If parameter match to redirectUrl return true, frontend url not provided',
+        () {
+      final isUrlRedirection = AuthCodeConfig(
+        redirectUrl: 'https://test.com',
+        clientId: 'N/A',
+      ).isCurrentUrlMatchToRedirection('https://test.com');
+
+      expect(isUrlRedirection, isTrue);
+    });
+
+    test('If parameter does not match to redirectUrl return false, frontend redirection not provided', () {
+      final isUrlRedirection = AuthCodeConfig(
+        redirectUrl: 'https://test.com',
+        clientId: 'N/A',
+      ).isCurrentUrlMatchToRedirection('https://something.com');
+
+      expect(isUrlRedirection, isFalse);
+    });
+
+    test('If parameter does not match to redirectUrl return false, frontend redirection matches', () {
+      final isUrlRedirection = AuthCodeConfig(
+        redirectUrl: 'https://test.com',
+        clientId: 'N/A',
+        frontendRedirectUrlParam: 'https://something.com',
+      ).isCurrentUrlMatchToRedirection('https://something.com');
+
+      expect(isUrlRedirection, isTrue);
+    });
+
+    test('If parameter does not match to redirectUrl return false, frontend redirection not matching', () {
+      final isUrlRedirection = AuthCodeConfig(
+        redirectUrl: 'https://test.com',
+        clientId: 'N/A',
+        frontendRedirectUrlParam: 'https://something.com',
+      ).isCurrentUrlMatchToRedirection('https://somethingelse.com');
+
+      expect(isUrlRedirection, isFalse);
+    });
+  });
 }
