cc2543c6a387d2ecf19b0c082bbdfcba6b0d813e
Nihad Delic <nihad@upday.com>
2020-12-04 02:30:53 +0100

    WIP

--- test/unit/src/DAL/api/exceptions_test.dart
+++ test/unit/src/DAL/api/exceptions_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:linkedin_login/redux/core.dart';
 import 'package:linkedin_login/src/DAL/api/exceptions.dart';
 
 void main() {
@@ -37,6 +36,17 @@ void main() {
       expect(
         () => AuthCodeException(authCode: 'authCode', description: null),
         throwsAssertionError,
+      );
+    });
+
+    test('test toString method', () async {
+      final exception =
+          AuthCodeException(authCode: 'authCode', description: 'description');
+      expect(
+        exception.toString(),
+        equals(
+          'AuthCodeException:: for authCode: authCode with description: description',
+        ),
       );
     });
   });
